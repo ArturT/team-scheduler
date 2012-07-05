522949d1c29478a9a876e92a1e45ce1e54e6e79b
Phillip Boksz <phillip.boksz@llp.pl>
2012-07-05 12:18:11 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamScheduler::Application.config.secret_token = 'fb20d999ca82c7c22cbac8c40135000f9977919041075c0f5076ff1a0cfef94ab241a2ee52f22ef2d919b4ae1c2a17d1de4ebc626dc020c8637e42acfefdf3f9'
