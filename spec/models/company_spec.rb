20792650d24803e7576f1f860393da0325e54246
Artur Trzop <artur.trzop@llp.pl>
2012-07-17 10:36:37 +0200

    Missing new lines

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -4,6 +4,7 @@ describe Company do
   it "validates presence of name" do
     build(:company, :name => '').should_not be_valid
   end
+
   it "validates presence of domain" do
     build(:company, :domain => '').should_not be_valid
   end
