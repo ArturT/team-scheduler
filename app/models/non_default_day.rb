afce47415f3bb0368002783e7af0a052286d5a4b
Artur Trzop <artur.trzop@llp.pl>
2012-07-20 17:01:01 +0200

    Add working hours scope validator to non default day model

--- app/models/non_default_day.rb
+++ app/models/non_default_day.rb
@@ -1,15 +1,10 @@
 class NonDefaultDay < ActiveRecord::Base
+  include ActiveModel::Validations
+
   belongs_to :schedule
   attr_accessible :date, :schedule_id, :hours
 
   validates :date, :presence => true
-  validates :hours, :presence => true
+  validates :hours, :presence => true, :working_hours_scope => true
   validates :schedule_id, :presence => true
-
-  # 0 = off day or sick day
-  # 2 = 1/4 day
-  # 4 = 1/2 day
-  # 6 = 3/4 day
-  # 8 = full day
-  validates_inclusion_of :hours, :in => (0..8).step(2)
 end
--- app/validators/working_hours_scope_validator.rb
+++ app/validators/working_hours_scope_validator.rb
@@ -1,9 +1,14 @@
 # See http://www.perfectline.ee/blog/building-ruby-on-rails-3-custom-validators
+# 0 = off day or sick day
+# 2 = 1/4 day
+# 4 = 1/2 day
+# 6 = 3/4 day
+# 8 = full day
 class WorkingHoursScopeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if not value.nil?
       unless (0..8).step(2).include?(value)
-       record.errors.add(attribute, "must be in scope 0, 2, 4, 6, 8.")
+        record.errors.add(attribute, "must be in scope 0, 2, 4, 6, 8.")
       end
     end
   end
