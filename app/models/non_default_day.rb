361ae9257cd51462c68804fb4e7e491ddd69f48b
Phillip Boksz <phillip.boksz@llp.pl>
2012-07-23 17:35:13 +0200

    Modify views to be more uniform

--- app/models/non_default_day.rb
+++ app/models/non_default_day.rb
@@ -7,15 +7,4 @@ class NonDefaultDay < ActiveRecord::Base
   validates :date, :presence => true
   validates :hours, :presence => true, :working_hours_scope => true
   validates :schedule_id, :presence => true
-  #validate :create_default_hour?
-
-  def create_default_hour?
-    if hours == schedule.default_hours
-      errors.add(:hours, 'have not been changed')
-    end
-  end
-
-  def is_default_hours?
-    hours == schedule.default_hours
-  end
 end
